3916748c74bedf3c46f2fbc470a30b0995041d9b
irina <magdici.irina@gmail.com>
2019-04-20 16:13:53 +0300

    test init change with us-east-1 region

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -5,12 +5,12 @@ const init = async () => {
         return
     }
 
-    process.env.restaurants_api   = "https://65mjm1mymh.execute-api.eu-west-1.amazonaws.com/dev/restaurants"
+    process.env.restaurants_api   = "https://5ibrt3dbz7.execute-api.us-east-1.amazonaws.com/dev/restaurants"
     process.env.restaurants_table = "restaurants-dev-irinaodobescu"
-    process.env.AWS_REGION        = "eu-west-1"
+    process.env.AWS_REGION        = "us-east-1"
     process.env.order_events_stream = 'orders-dev-irinaodobescu'
     process.env.restaurant_notification_topic = 'restaurants-dev-irinaodobescu'
-    process.env.TEST_ROOT = "https://65mjm1mymh.execute-api.eu-west-1.amazonaws.com/dev"
+    process.env.TEST_ROOT = "https://5ibrt3dbz7.execute-api.us-east-1.amazonaws.com/dev"
 
     initialized = true
 }
